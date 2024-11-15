5369a198a450673fd2298787faeb1747c82cddb0
Rodrigo-SE03 <se03rodrigo@gmail.com>
2024-11-15 14:06:22 -0300

    batalha naval

---
+++ Trabalhos/N2/funcoes.py
@@ -0,0 +1,41 @@
+import random
+
+def mostrar_tabuleiro(tabuleiro):
+    linhas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+    print('  1 2 3 4 5 6 7 8 9 10')
+    for i in range(0, 10):
+        print(linhas[i], end=' ')
+        for j in range(0, 10):
+            print(tabuleiro[i][j], end=' ')
+        print()
+
+def posicionar_barcos(tabuleiro, barcos):
+    for barco, tamanho in barcos.items():
+        coincidente = True
+        print('Posicione o', barco)
+        orientacao = random.choice(['v', 'h'])
+        if orientacao == 'v':
+            while coincidente == True:
+                x = random.randint(0, 9)
+                y = random.randint(0, 9 - tamanho)
+                for i in range(0, tamanho):
+                    if tabuleiro[y + i][x] == 1:
+                        coincidente = True
+                    else:
+                        coincidente = False
+                if coincidente == False:
+                    for i in range(0, tamanho):
+                        tabuleiro[y + i][x] = 1
+        else:
+            while coincidente == True:
+                x = random.randint(0, 9 - tamanho)
+                y = random.randint(0, 9)
+                for i in range(0, tamanho):
+                    if tabuleiro[y][x + i] == 1:
+                        coincidente = True
+                    else:
+                        coincidente = False
+                if coincidente == False:
+                    for i in range(0, tamanho):
+                        tabuleiro[y][x + i] = 1
+    return tabuleiro
---
+++ Trabalhos/N2/main.py
@@ -0,0 +1,59 @@
+import random
+from funcoes import mostrar_tabuleiro, posicionar_barcos
+import os
+
+tabuleiro_inimigo = [[]]
+tabuleiro_visivel = [[]]
+linhas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+for i in range(0,10):
+    tabuleiro_inimigo.append([])
+    tabuleiro_visivel.append([])
+    for j in range(0,10):
+        tabuleiro_inimigo[i].append(0)
+        tabuleiro_visivel[i].append("~")
+tabuleiro_inimigo.pop()
+tabuleiro_visivel.pop()
+
+barcos = {
+    "porta_avioes": 5,
+    "navio_de_guerra": 4,
+    "submarino": 3,
+    "contratorpedeiro": 3,
+    "patruha": 2
+}
+total = 0
+for barco in barcos:
+    total += barcos[barco]
+
+tabuleiro_inimigo = posicionar_barcos(tabuleiro_inimigo, barcos)
+
+# Usar para tomar spoiler
+# mostrar_tabuleiro(tabuleiro_inimigo)
+invalido = False
+while True:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    if invalido:
+        print("Coordenada inválida!")
+        invalido = False
+    mostrar_tabuleiro(tabuleiro_visivel)
+    tiro = input("Digite a coordenada do tiro (ex: A1): ")
+    try:
+        y = tiro[0].upper()
+        x = int(tiro[1:]) - 1
+    except:
+        invalido = True
+        continue
+    if x>10 or x<0 or y not in linhas:
+        invalido = True
+        continue
+    y = linhas.index(y)
+    if tabuleiro_inimigo[y][x] == 1:
+        tabuleiro_visivel[y][x] = "X"
+        total -= 1
+    else:
+        tabuleiro_visivel[y][x] = "O"
+
+    if total == 0:
+        print("Você ganhou!")
+        mostrar_tabuleiro(tabuleiro_visivel)
+        break
